df9ee8540aaf3a44d37227ea8582286049c325c9
pagiestm <pagiestm@gmail.com>
2022-01-23 12:04:34 +0100

    modification

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function verification() {
         return true;
     }
 }
+function checkEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+function verification(){
+    var email = document.getElementById("email").value;
+         
+    if (checkEmail(email)) {
+        alert('Adresse e-mail valide');
+        return true;
+    } else {
+        alert('Adresse e-mail non valide');
+        return false;
+    }
+}
