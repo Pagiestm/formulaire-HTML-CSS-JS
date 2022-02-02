985369cbee152f7db05bde0ea3c471402158627c
pagiestm <pagiestm@gmail.com>
2022-02-02 16:53:47 +0100

    mot de passe, login, email, checkbox

--- script.js
+++ script.js
@@ -4,23 +4,10 @@ function verification() {
         alert("Attention : Votre login doit contenir 8 lettres au minimum !");
         return false;
     }
-}
-
-function verification(){
     if (document.getElementById("pass").value != document.getElementById("confirm_password").value) {
         alert("Les mots de passe ne correspondent pas");
-        return true;
-    }
-    else {
         return false;
     }
-}
-
-function checkEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-function verification() {
     var email = document.getElementById("email").value;
 
     if (checkEmail(email)) {
@@ -30,6 +17,11 @@ function verification() {
         alert('Adresse e-mail non valide');
         return false;
     }
+}
+
+function checkEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
 
 function check() {
