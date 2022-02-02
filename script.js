ce1c860b10531c530a9d4270be3a77ee16108243
pagiestm <pagiestm@gmail.com>
2022-02-02 15:00:27 +0100

    nouveau

--- script.js
+++ script.js
@@ -4,26 +4,40 @@ function verification() {
         alert("Attention : Votre login doit contenir 8 lettres au minimum !");
         return false;
     }
+}
+
+function verification(){
     if (document.getElementById("pass").value != document.getElementById("confirm_password").value) {
         alert("Les mots de passe ne correspondent pas");
-        return false;
-    }
-    else {
         return true;
     }
+    else {
+        return false;
+    }
 }
+
 function checkEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
-function verification(){
+function verification() {
     var email = document.getElementById("email").value;
-         
+
     if (checkEmail(email)) {
-        alert('Adresse e-mail valide');
         return true;
-    } else {
+    }
+    else{
         alert('Adresse e-mail non valide');
         return false;
+    }
+}
+
+function check() {
+    CheckBox = document.getElementById("checkbox");
+    if (CheckBox.checked) {
+        document.getElementById("choisir").style.display = "block";
+    }
+    else {
+        document.getElementById("choisir").style.display = "none";
     }
 }
